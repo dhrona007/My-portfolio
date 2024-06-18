5340f36cf63f4485304864e2d80f39e95828d721
dhrona007 <136801950+dhrona007@users.noreply.github.com>
2024-06-18 21:50:24 +0530

    Add files via upload
    Added an HTML, CSS & JavaScript files of my portfolio which are the overview of my portfolio website

---
+++ js.js
@@ -0,0 +1,28 @@
+document.getElementById('b1').addEventListener('click', function()
+        {
+        document.getElementById('about').scrollIntoView();
+        });
+        
+        //summary section animation
+        document.getElementById('b2').addEventListener('click', function()
+        {
+        document.getElementById('su').scrollIntoView();
+        });
+        
+        //skills section animation
+        document.getElementById('b3').addEventListener('click', function()
+        {
+        document.getElementById('sk').scrollIntoView();
+        });
+        
+        //projects section animation
+        document.getElementById('b4').addEventListener('click', function()
+        {
+        document.getElementById('p').scrollIntoView();
+        });
+        
+        //contact section animation
+        document.getElementById('b1').addEventListener('click', function()
+        {
+        document.getElementById('c').scrollIntoView();
+        });
